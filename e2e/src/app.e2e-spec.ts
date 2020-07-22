89df51155a7855a8dfd0d507fd218a9d46322163
alexandrumanta13 <alexandru.manta@hotmail.com>
2020-07-22 19:59:04 +0300

    chore: replace the name with the project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kiaz-ng app is running!');
+    expect(page.getTitleText()).toEqual('allsteam-ng app is running!');
   });
 
   afterEach(async () => {
